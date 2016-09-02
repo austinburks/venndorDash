77cf8a0193d4170e9c60e5b7a11aa850124656e2
austinburks <austinburks.ab@gmail.com>
2016-09-02 10:12:38 -0400

    Added additional app route

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    i = 1
+    i += 1
+    return "Hello!!!"
+    
+@app.route('/pg2')
+def hello_again():
+    return "hello again!"
 
 if __name__ == '__main__':
     app.run(debug=True)
