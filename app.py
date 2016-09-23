a9b41f60e7528ff749e907462c097e7185c4fb09
Austin Burks <austinburks.ab@gmail.com>
2016-09-22 20:46:08 -0400

    Commiting files

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
     i = 1
     i += 1
     i += 5
-    x = (os.environ['APP_SETTINGS'])    
+    #x = (os.environ['APP_SETTINGS'])    
 
-    return "Hello!!! %s" %x
+    return "Hello!!! %s"
     
 @app.route('/pg2')
 def hello_again():
     return "hello again!"
+
+@app.route('/hiChris')
+def hichris():
+    return "hi Chris!"
 
 if __name__ == '__main__':
     app.run(debug=True)
